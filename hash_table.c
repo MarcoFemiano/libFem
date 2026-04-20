fecac9e514fdc583d50ac46a402400de73ae4a00
MarcoFemiano <perikar13@gmail.com>
2026-04-20 17:41:41 +0200

    Update hash_table.c

--- hash_table.c
+++ hash_table.c
@@ -7,7 +7,7 @@
 #include <stddef.h>
 
 struct strHashTable {
-  void** data;
+  unsigned long long int* data;
   size_t size;
   size_t capacity;
   size_t defaultCapacity;
@@ -18,6 +18,7 @@ status_codes hashTable_create(HashTable* hashTable,size_t defaultCapacity,size_t
   //tests di robustezza
   if (hashTable == NULL) return ERROR_NULL_POINTER;
   if (sizeOfElements == 0) return ERROR_WRONG_ELEMENTS_SIZE;
+  if (defaultCapacity == 0) return ERROR_ALLOCATION_FAILURE;
 
   HashTable newHashTable = malloc (sizeof(struct strHashTable));
   if (newHashTable == NULL) return ERROR_ALLOCATION_FAILURE;
@@ -27,23 +28,10 @@ status_codes hashTable_create(HashTable* hashTable,size_t defaultCapacity,size_t
   newHashTable->defaultCapacity = defaultCapacity;
   newHashTable->sizeOfelements = sizeOfElements;
 
-  if (sizeOfElements > SIZE_MAX / newHashTable->capacity) return ERROR_ARITHMETIC_OVERFLOW;
+  if (defaultCapacity > SIZE_MAX / sizeof(unsigned long long int))
 
-  newHashTable->data = malloc (newHashTable->capacity * sizeOfElements);
-  for (size_t i = 0; i < newHashTable->capacity; i++) {
-    newHashTable->data[i] = malloc(sizeOfElements);
-    if (newHashTable->data[i] == NULL){
-      {
-        while (i>0) {
-          free(newHashTable->data[i-1]);
-          i--;
-        }
-        free(newHashTable->data);
-        free(newHashTable);
-      }
-        return ERROR_ALLOCATION_FAILURE;
-      }
-  }
+  newHashTable->data = malloc (newHashTable->capacity * sizeof(unsigned long long int));
+
   if (newHashTable->data == NULL) {
     free(newHashTable);
     return ERROR_ALLOCATION_FAILURE;
@@ -56,11 +44,8 @@ status_codes hashTable_create(HashTable* hashTable,size_t defaultCapacity,size_t
 
 status_codes hashTable_destroy(HashTable* hashTable) {
   //tests di robustezza
-  if (hashTable == NULL || *hashTable == NULL || (*hashTable)->data) return ERROR_NULL_POINTER;
+  if (hashTable == NULL || *hashTable == NULL || (*hashTable)->data == NULL) return ERROR_NULL_POINTER;
 
-  for (size_t i = 0; i< (*hashTable)->size; i++) {
-    if ((*hashTable)->data[i] != NULL) free((*hashTable)->data[i]);
-  }
   free((*hashTable)->data);
   free(*hashTable);
   *hashTable = NULL;
@@ -77,11 +62,12 @@ static status_codes hashTable_makeHash(HashTable hashTable, void* value,unsigned
 }
 
 
+//non va bene. da rifare
 static status_codes hashTable_rehash(HashTable hashTable) {
   if (hashTable == NULL || hashTable->data == NULL) return ERROR_NULL_POINTER;
 
   for (size_t i = 0; i < hashTable->size; i++) {
-      unsigned long long int hash = XXH3_64bits(&hashTable->data[i],hashTable->sizeOfelements);
+      unsigned long long int hash = XXH3_64bits(&hashTable->data[i],sizeof(unsigned long long int));
       hash = hash % hashTable->capacity;
       status_codes res = hashTable_push(hashTable, &hash);
       if (res != OK) return res;
@@ -92,26 +78,29 @@ static status_codes hashTable_rehash(HashTable hashTable) {
 
 static status_codes hashTable_adjustCapacity(HashTable hashTable) {
   if (hashTable == NULL || hashTable->data == NULL) return ERROR_NULL_POINTER;
+
+  size_t newCapacity = 0;
   //espansione
   if (hashTable->size == hashTable->capacity) {
-    size_t newCapacity = hashTable->capacity * HASH_TABLE_INCREMENT_MULTIPLIER;
+    newCapacity = hashTable->capacity * HASH_TABLE_INCREMENT_MULTIPLIER;
     if (newCapacity  > SIZE_MAX / hashTable->capacity ) return ERROR_ARITHMETIC_OVERFLOW;
 
-    void* temp = realloc(hashTable->data,newCapacity);
+    void* temp = realloc(hashTable->data,newCapacity*sizeof(unsigned long long int));
     if (temp == NULL) return ERROR_ALLOCATION_FAILURE;
 
     hashTable->data = temp;
   }//riduzione
   else if (hashTable->size < hashTable->capacity/HASH_TABLE_INCREMENT_MULTIPLIER) {
 
-    size_t newCapacity = hashTable->capacity / HASH_TABLE_INCREMENT_MULTIPLIER;
-    void* temp = realloc(hashTable->data,newCapacity);
+    newCapacity = hashTable->capacity / HASH_TABLE_INCREMENT_MULTIPLIER;
+    void* temp = realloc(hashTable->data,newCapacity*sizeof(unsigned long long int));
     if (temp == NULL) return ERROR_ALLOCATION_FAILURE;
-    hashTable->data[hashTable->size] = temp;
+    hashTable->data = temp;
+
 
   }
 
-
+  hashTable->capacity = newCapacity;
   return OK;
 }
 
@@ -125,7 +114,7 @@ status_codes hashTable_push(HashTable hashTable, void* value) {
   if (res != OK) return res;
 
   hash = hash % hashTable->capacity;
-  hashTable->data[hashTable->size] = &hash;
+  hashTable->data[hashTable->size] = hash;
   hashTable->size++;
   return OK;
 }
